5aef2636071098bbac1d8c1e7c52ffd6d2ce8ebb
front-end-captain <18810940932@163.com>
2020-03-15 19:38:00 +0800

    chore(cli): add about test scripts description in readme file

--- packages/@luban/cli/dist/utils/getReadme.js
+++ packages/@luban/cli/dist/utils/getReadme.js
@@ -1,20 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const descriptions = {
+const scriptsNameDescriptions = {
     start: "alias of serve",
     build: "Compiles and minifies for production",
     serve: "Compiles and hot-reloads for development",
-    eslint: "find problems in you code",
-    "eslint:fix": "fix problems in you code",
-    stylelint: "find problems in you style code",
-    inspect: "inspect webpack config is specify [mode]",
+    eslint: "Find problems in you code",
+    "eslint:fix": "Fix problems in you code",
+    stylelint: "Find problems in you style code",
+    inspect: "Inspect webpack config is specify [mode]",
+    test: "Run all unit tests",
+    "test:update": "Re-record every snapshot that fails during this test run",
+    "test:coverage": "Indicates that test coverage information should be collected and reported in the output",
 };
 function printScripts(pkg, packageManager) {
     return Object.keys(pkg.scripts || {})
         .map((key) => {
-        if (!descriptions[key])
+        if (!scriptsNameDescriptions[key])
             return "";
-        return [`\n### ${descriptions[key]}`, "```", `${packageManager} run ${key}`, "```", ""].join("\n");
+        return [
+            `\n### ${scriptsNameDescriptions[key]}`,
+            "```",
+            `${packageManager} run ${key}`,
+            "```",
+            "",
+        ].join("\n");
     })
         .join("");
 }
--- packages/@luban/cli/src/utils/getReadme.ts
+++ packages/@luban/cli/src/utils/getReadme.ts
@@ -1,22 +1,30 @@
 import { SUPPORTED_PACKAGE_MANAGER, BasePkgFields } from "../definitions";
 
-const descriptions = {
+const scriptsNameDescriptions = {
   start: "alias of serve",
   build: "Compiles and minifies for production",
   serve: "Compiles and hot-reloads for development",
-  eslint: "find problems in you code",
-  "eslint:fix": "fix problems in you code",
-  stylelint: "find problems in you style code",
-  inspect: "inspect webpack config is specify [mode]",
+  eslint: "Find problems in you code",
+  "eslint:fix": "Fix problems in you code",
+  stylelint: "Find problems in you style code",
+  inspect: "Inspect webpack config is specify [mode]",
+  test: "Run all unit tests",
+  "test:update": "Re-record every snapshot that fails during this test run",
+  "test:coverage":
+    "Indicates that test coverage information should be collected and reported in the output",
 };
 
 function printScripts(pkg: BasePkgFields, packageManager: SUPPORTED_PACKAGE_MANAGER): string {
   return Object.keys(pkg.scripts || {})
     .map((key) => {
-      if (!descriptions[key]) return "";
-      return [`\n### ${descriptions[key]}`, "```", `${packageManager} run ${key}`, "```", ""].join(
-        "\n",
-      );
+      if (!scriptsNameDescriptions[key]) return "";
+      return [
+        `\n### ${scriptsNameDescriptions[key]}`,
+        "```",
+        `${packageManager} run ${key}`,
+        "```",
+        "",
+      ].join("\n");
     })
     .join("");
 }
