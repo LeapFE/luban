cd6a1e03544a352a63dbfd7c5394928d433334ed
front-end-captain <18810940932@163.com>
2021-02-13 16:57:23 +0800

    build: modify commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "subject-max-length": [2, "always", [80]],
+    "header-max-length": [2, "always", [100]],
   },
 };
