b0db70dfa6714d1ca7d85bac846038594079fe9c
front-end-captain <18810940932@163.com>
2021-03-15 18:04:25 +0800

    feat(unit-test): add example of unit-test in template

--- packages/@luban/cli-plugin-unit-test/src/generator.ts
+++ packages/@luban/cli-plugin-unit-test/src/generator.ts
@@ -10,23 +10,22 @@ export default function(api: GeneratorAPI, options: Required<RootOptions>): void
       "test:update": "jest -u",
       "test:coverage": "jest --coverage --color",
     },
+    dependencies: {
+      classnames: "^2.2.6",
+    },
     devDependencies: {
+      "@types/classnames": "^2.2.11",
+      "@types/enzyme": "^3.10.5",
+      "@types/enzyme-adapter-react-16": "^1.0.6",
+      "@types/jest": "^25.1.4",
+      "@types/enzyme-to-json": "^1.5.3",
+      "@types/react-test-renderer": "^16.9.3",
       enzyme: "^3.11.0",
       "enzyme-adapter-react-16": "^1.15.2",
       jest: "^25.1.0",
       "enzyme-to-json": "^3.4.4",
       "react-test-renderer": "^16.14.0",
-    },
-  });
-
-  api.extendPackage({
-    devDependencies: {
-      "@types/enzyme": "^3.10.5",
-      "@types/enzyme-adapter-react-16": "^1.0.6",
-      "@types/jest": "^25.1.4",
-      "@types/enzyme-to-json": "^1.5.3",
       "ts-jest": "^25.2.1",
-      "@types/react-test-renderer": "^16.9.3",
     },
   });
 
