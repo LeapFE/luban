58e24048443d3bbea2b3b851168689d7ebd07a4f
front-end-captain <18810940932@163.com>
2021-04-20 22:54:38 +0800

    fix(babel): add 'react-refresh/babel' plugin just in 'development' env in babel.config.js

--- packages/@luban/cli-plugin-babel/src/generator.ts
+++ packages/@luban/cli-plugin-babel/src/generator.ts
@@ -2,27 +2,13 @@ import { GeneratorAPI } from "@luban-cli/cli-shared-types/dist/cli/lib/generator
 // import { RootOptions } from "@luban-cli/cli-shared-types/dist/shared";
 
 export default function(api: GeneratorAPI): void {
-  const babelConfigPreset: Array<string | Array<string | Record<string, unknown>>> = [
-    [
-      "@babel/preset-env",
-      { modules: false, useBuiltIns: "usage", corejs: { version: 3, proposals: true } },
-    ],
-    "@babel/preset-react",
-    "@babel/preset-typescript",
-  ];
-
-  const babelConfigPlugins: Array<string | Array<string | Record<string, unknown>>> = [
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-transform-runtime",
-    "react-refresh/babel",
-  ];
-
   api.extendPackage({
     devDependencies: {
       "@babel/cli": "^7.13.14",
       "@babel/core": "^7.13.15",
       "@babel/plugin-transform-runtime": "^7.13.15",
       "@babel/preset-env": "^7.13.15",
+      "@babel/preset-typescript": "^7.8.0",
       "@babel/runtime": "^7.13.10",
       "@babel/preset-react": "^7.13.13",
       "core-js": "^3.10.1",
@@ -32,14 +18,5 @@ export default function(api: GeneratorAPI): void {
     },
   });
 
-  api.extendPackage({
-    devDependencies: {
-      "@babel/preset-typescript": "^7.8.0",
-    },
-  });
-
-  api.render("./template", {
-    presets: JSON.stringify(babelConfigPreset),
-    plugins: JSON.stringify(babelConfigPlugins),
-  });
+  api.render("./template");
 }
