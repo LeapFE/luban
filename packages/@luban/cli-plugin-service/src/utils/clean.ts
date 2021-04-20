ef3b01db01c541278545d52d36769a5d32f39760
front-end-captain <18810940932@163.com>
2021-04-20 23:03:28 +0800

    fix(cli-plugin-service): catch exception while clean dest files

--- packages/@luban/cli-plugin-service/src/utils/clean.ts
+++ packages/@luban/cli-plugin-service/src/utils/clean.ts
@@ -1,9 +1,15 @@
 import fs from "fs-extra";
 import path from "path";
+import { error } from "@luban-cli/cli-shared-utils";
 
 export async function cleanDest(context: string, outputDir: string) {
   const _path = path.resolve(context, outputDir);
-  if (fs.pathExistsSync(_path)) {
-    await fs.remove(_path);
+
+  try {
+    if (fs.pathExistsSync(_path)) {
+      await fs.remove(_path);
+    }
+  } catch (e) {
+    error("Try clean dest files failed", e);
   }
 }
