9a21f32610f1c9df8d5ab2c8f435abe57fe43290
front-end-captain <18810940932@163.com>
2021-06-08 23:43:23 +0800

    build(doc): use 'koa' to serve doc

--- doc/server/index.js
+++ doc/server/index.js
@@ -4,10 +4,6 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist"));
 
-app.use("/", (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html");
-})
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening up at ${port}`);
---
+++ doc/server/koa.js
@@ -0,0 +1,11 @@
+const Koa = require("koa");
+const static = require("koa-static");
+
+const app = new Koa();
+
+app.use(static("dist"))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening up at ${port}`);
+});
