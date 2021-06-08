8278330d3f05fcc959a8d931d0ffce1a3e4a9ca5
front-end-captain <18810940932@163.com>
2021-06-08 23:06:53 +0800

    build(doc): server entry file for doc

--- doc/server/index.js
+++ doc/server/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist"));
 
+app.use("/", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening up at ${port}`);
