0ba66b8b4df66f51f7bf057a9d626fb5a53de2ef
front-end-captain <18810940932@163.com>
2021-06-08 22:16:19 +0800

    build(doc): update server entry file for doc

--- doc/server/index.js
+++ doc/server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("dist"));
+app.use(express.static(__dirname + "/dist"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
